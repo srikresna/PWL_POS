29e5c1da4e15cfffd32ec2f7adde1b47afeba650
srikresna <srikresna383@gmail.com>
2024-03-01 15:56:43 +0700

    feat: query builder for KategoriController 🕹

---
+++ resources/views/kategori.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Data Level Pengguna</title>
+    </head>
+    <body>
+        <h1>Data Level Pengguna</h1>
+        <table border="1" cellpadding="2" cellspacing="0">
+            <tr>
+                <th>ID</th>
+                <th>Kode Kategori</th>
+                <th>Nama Kategori</th>
+            </tr>
+            @foreach ( $data as $d )
+            <tr>
+                <td>{{ $d->kategori_id }}</td>
+                <td>{{ $d->kategori_kode }}</td>
+                <td>{{ $d->kategori_nama }}</td>
+            </tr>
+            @endforeach
+        </table>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\KategoriController;
 use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
@@ -18,3 +19,4 @@ Route::get('/', function () {
 });
 
 Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
