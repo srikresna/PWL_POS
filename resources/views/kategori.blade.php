c8c0af39c1c33d8dd96f59e3f756f09735791533
srikresna <srikresna383@gmail.com>
2024-03-02 18:21:51 +0700

    feat: ORM implementations 📱

--- resources/views/kategori.blade.php
+++ resources/views/kategori.blade.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Data Level Pengguna</title>
+        <title>Data Kategori</title>
     </head>
     <body>
-        <h1>Data Level Pengguna</h1>
+        <h1>Data Kategori</h1>
         <table border="1" cellpadding="2" cellspacing="0">
             <tr>
                 <th>ID</th>
