9d835f693ddd2691695dd8c4c399389dbd002dd2
srikresna <srikresna383@gmail.com>
2024-03-14 16:13:42 +0700

    feat: add fillable 🔥

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -24,11 +24,19 @@ class UserController extends Controller
 
         // UserModel::insert($data); // tambahkan data ke tabel m_user
 
-        $data = [
-            'nama' => 'Pelanggan Pertama',
-        ];
+        // $data = [
+        //     'nama' => 'Pelanggan Pertama',
+        // ];
 
-        UserModel::where('username', 'customer-1')->update($data); // update data ke tabel m_user
+        // UserModel::where('username', 'customer-1')->update($data); // update data ke tabel m_user
+
+        $data = [
+            'level_id' => 2,
+            'username' => 'manager_dua',
+            'nama' => 'Manager 2',
+            'password' => Hash::make('12345')
+        ];
+        UserModel::create($data); // tambahkan data ke tabel m_user
 
         // coba akses model UserModel
         $user = UserModel::all(); // ambil semua data dari tabel m_user
