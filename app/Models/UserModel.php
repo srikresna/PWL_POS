13492496956ee09f759d87873ceb2e490d5defef
srikresna <srikresna383@gmail.com>
2024-05-09 23:30:20 +0700

    feat: practicum 2 jb10

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -3,16 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
-use Illuminate\Auth\Authenticatable;
+// use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class UserModel extends Model implements JWTSubject
+class UserModel extends Authenticatable implements JWTSubject
 {
     // use HasFactory, Authenticatable;
     
+    use HasFactory;
+
     public function getJWTIdentifier(){
         return $this->getKey();
     }
